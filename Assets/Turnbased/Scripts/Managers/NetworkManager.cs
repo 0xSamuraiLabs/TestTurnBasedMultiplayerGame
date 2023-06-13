4b2c36fc3ff41bda811d57c205fbf6903abe0c47
dev <developer@0xsamurai.com>
2023-06-13 19:41:02 +0545

    added lobby system

---
+++ Assets/Turnbased/Scripts/Managers/NetworkManager.cs
@@ -0,0 +1,11 @@
+using Photon.Pun;
+
+namespace Turnbased.Scripts.Managers
+{
+    public class NetworkManager : MonoBehaviourPunCallbacks
+    {
+    
+        
+        
+    }
+}
---
+++ Assets/Turnbased/Scripts/Managers/PhotonManager.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private GameObject matchingPanel;
+    [SerializeField] private TMP_Text matchingText;
+    [SerializeField] private Button searchButton;
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        searchButton.interactable = true;
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        if (matchingPanel)
+        {
+            matchingPanel.SetActive(false);
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+        matchingPanel.SetActive(true);
+        CheckForMaxPlayers();
+    }
+
+    public void LoadScene()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        PhotonNetwork.CurrentRoom.MaxPlayers = 2;
+        matchingPanel.SetActive(true);
+        matchingText.text = "Searching For Opponent...";
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        base.OnPlayerEnteredRoom(newPlayer);
+        CheckForMaxPlayers();
+    }
+
+    void CheckForMaxPlayers()
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
+        {
+            Debug.Log("Max Players Load another scene");
+            matchingText.text = "Opponent Found";
+            Invoke(nameof(LoadScene),1f);
+        }
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        matchingText.text = "Searching For Opponent...";
+        matchingPanel.SetActive(false);
+    }
+
+    public void JoinLobby()
+    {
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public void CancelSearch()
+    {
+        matchingPanel.SetActive(false);
+        PhotonNetwork.LeaveRoom();
+    }
+    
+}
